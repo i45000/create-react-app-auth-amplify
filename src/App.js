62b9515debb6d4e9bf3b73c49eb1a2ddaa61c702
EC2 Default User <ec2-user@ip-172-31-28-249.ap-southeast-1.compute.internal>
2019-10-22 09:52:46 +0000

    FYP1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -10,20 +8,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div class="header">
+        <h1>IVE IA Document Generator</h1>
+        <a class="credit">All credit goes to: Mark Lai IT114115/2B 2017-2018 Wan Ka Chi IT114115/2A 2019-2020 Wong Hok
+            Leung IT114115/2A 2019-2020</a>
+    </div>
+    <div class="part">
+        <h4>Instruction</h4>
+        <div class="inner">
+            <div class="warning">[ ! Important ! ]</div>
+            If you are running at local machine, make sure you see the following message on chrome :
+            <div class="warning-box">You are using a unsupported command-line flag: --disable-web-security</div>
+            If not, Make sure you are running this file through run.bat with admin right<br />
+            &nbsp;&nbsp;<br /><br /><br />
+
+            1. Fill in this form<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;or<br />
+            &nbsp;&nbsp;&nbsp;Load existing json from below (If you have)<br /><br />
+
+            2. Press "Generate IA Doc"<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;or<br />
+            &nbsp;&nbsp;&nbsp;"save as JSON" (If you want to edit the info later)<br /><br />
+
+            3. Sign the IA document at page 6, 7, 11, 13, 14, 16, 18
+        </div>
+    </div>
+    <div class="part">
+        <h4>Read existing JSON file</h4>
+        <div class="inner">
+            <input type="file" id="fileToLoad" />
+            <button onclick="loadJSON()">Load JSON</button>
+        </div>
+    </div>
       </div>
     );
   }
